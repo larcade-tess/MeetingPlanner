6bbddfd13409db37d42aaec8e4a26893ec06b348
Ji, Jessie <asaggo@byui.edu>
2018-04-10 16:32:53 -0600

    able to add speakers. (new speaker page is added)

---
+++ MeetingPlanner/Controllers/SpeakersController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MeetingPlanner.Data;
+using MeetingPlanner.Models;
+
+namespace MeetingPlanner.Controllers
+{
+    public class SpeakersController : Controller
+    {
+        private readonly MeetingContext _context;
+
+        public SpeakersController(MeetingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Speakers
+        public async Task<IActionResult> Index()
+        {
+            var meetingContext = _context.Speakers.Include(s => s.Bulletin);
+            return View(await meetingContext.ToListAsync());
+        }
+
+        // GET: Speakers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speaker = await _context.Speakers
+                .Include(s => s.Bulletin)
+                .SingleOrDefaultAsync(m => m.SpeakerID == id);
+            if (speaker == null)
+            {
+                return NotFound();
+            }
+
+            return View(speaker);
+        }
+
+        // GET: Speakers/Create
+        public IActionResult Create()
+        {
+            ViewData["BulletinID"] = new SelectList(_context.Bulletins, "ID", "ID");
+            return View();
+        }
+
+        // POST: Speakers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SpeakerID,BulletinID,Name,Topic")] Speaker speaker)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(speaker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BulletinID"] = new SelectList(_context.Bulletins, "ID", "ID", speaker.BulletinID);
+            return View(speaker);
+        }
+
+        // GET: Speakers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speaker = await _context.Speakers.SingleOrDefaultAsync(m => m.SpeakerID == id);
+            if (speaker == null)
+            {
+                return NotFound();
+            }
+            ViewData["BulletinID"] = new SelectList(_context.Bulletins, "ID", "ID", speaker.BulletinID);
+            return View(speaker);
+        }
+
+        // POST: Speakers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SpeakerID,BulletinID,Name,Topic")] Speaker speaker)
+        {
+            if (id != speaker.SpeakerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(speaker);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SpeakerExists(speaker.SpeakerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BulletinID"] = new SelectList(_context.Bulletins, "ID", "ID", speaker.BulletinID);
+            return View(speaker);
+        }
+
+        // GET: Speakers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var speaker = await _context.Speakers
+                .Include(s => s.Bulletin)
+                .SingleOrDefaultAsync(m => m.SpeakerID == id);
+            if (speaker == null)
+            {
+                return NotFound();
+            }
+
+            return View(speaker);
+        }
+
+        // POST: Speakers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var speaker = await _context.Speakers.SingleOrDefaultAsync(m => m.SpeakerID == id);
+            _context.Speakers.Remove(speaker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SpeakerExists(int id)
+        {
+            return _context.Speakers.Any(e => e.SpeakerID == id);
+        }
+    }
+}
