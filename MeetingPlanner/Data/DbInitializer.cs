710d47d7a41bdddb1ad566a5f86ef086a381126e
Tess Larcade <tesslarcade@gmail.com>
2018-04-10 12:08:23 -0600

    DB is seeding correctly, fixed the not adding multiple meeting problem

--- MeetingPlanner/Data/DbInitializer.cs
+++ MeetingPlanner/Data/DbInitializer.cs
@@ -17,26 +17,26 @@ namespace MeetingPlanner.Data
             {
                 return;   // DB has been seeded
             }
-            /*
-                     var bulletins = new Bulletin[]
-                        {
-                        new Bulletin{MeetingConductor="Bishop",OpeningSongNumber=300,OpeningPrayerName="Brother Hill",ClosingSongNumber=152,ClosingPrayerName="Sister Hill",MeetingDate=DateTime.Parse("2018-04-08")},
-                        new Bulletin{MeetingConductor="First Counceler",OpeningSongNumber=46,OpeningPrayerName="Sister Baker",ClosingSongNumber=12,ClosingPrayerName="Brother Lynn",MeetingDate=DateTime.Parse("2018-04-15")},
-                        new Bulletin{MeetingConductor="Second Counceler",OpeningSongNumber=204,OpeningPrayerName="Brother Allen",ClosingSongNumber=97,ClosingPrayerName="Brother Davidson",MeetingDate=DateTime.Parse("2018-04-22")}
-                        };
 
-                        foreach (Bulletin b in bulletins)
-                        {
-                            context.Bulletins.Add(b);
-                        }
-                        context.SaveChanges();
-                        */
+            var bulletins = new Bulletin[]
+               {
+                        new Bulletin{MeetingConductor="Bishop",OpeningSongNumber=300,SacramentSongNumber=21,OpeningPrayerName="Brother Hill",ClosingSongNumber=152,ClosingPrayerName="Sister Hill",MeetingDate=DateTime.Parse("2018-04-08")},
+                        new Bulletin{MeetingConductor="First Counceler",OpeningSongNumber=46,SacramentSongNumber=5,OpeningPrayerName="Sister Baker",ClosingSongNumber=12,ClosingPrayerName="Brother Lynn",MeetingDate=DateTime.Parse("2018-04-15")},
+                        new Bulletin{MeetingConductor="Second Counceler",OpeningSongNumber=204,SacramentSongNumber=75,OpeningPrayerName="Brother Allen",ClosingSongNumber=97,ClosingPrayerName="Brother Davidson",MeetingDate=DateTime.Parse("2018-04-22")}
+               };
+
+            foreach (Bulletin b in bulletins)
+            {
+                context.Bulletins.Add(b);
+            }
+            context.SaveChanges();
+
 
             var speakers = new Speaker[]
             {
-            new Speaker{BulletinID=0,Name="Sister Bradshaw",Topic="Prayer"},
-            new Speaker{BulletinID=0,Name="Sister Smith",Topic="Tithing"},
-            new Speaker{BulletinID=0,Name="Brother Calloway",Topic="Miracles"}
+            new Speaker{BulletinID=1,Name="Sister Bradshaw",Topic="Prayer"},
+            new Speaker{BulletinID=1,Name="Sister Smith",Topic="Tithing"},
+            new Speaker{BulletinID=2,Name="Brother Calloway",Topic="Miracles"}
             };
             foreach (Speaker s in speakers)
             {
